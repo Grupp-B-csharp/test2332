b373f9c177c8a5c360f2a2e4fc87c37f5519a178
Andreas Ekman <adde930505@hotmail.com>
2025-09-01 13:48:36 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace test2332.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
